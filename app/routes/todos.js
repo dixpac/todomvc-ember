630686e8ffe6ded62e687df7811df2178d0d90f8
dixpac <dino.onex@gmail.com>
2016-04-10 14:52:14 +0200

    Add mirage as dummy data backend

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,22 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let todos = [
-      {
-        title: 'Buy book',
-        complete: false
-      },
-      {
-        title: 'Play with puppiws',
-        complete: true
-      },
-      {
-        title: 'Watch netflix',
-        complete: false
-      }
-    ];
-    return todos;
-
+		return this.store.findAll('todo');
   }
 });
 

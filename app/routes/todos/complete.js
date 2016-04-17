d6ca41ca62e4be44efa112982f348bf7fe957be6
dixpac <dino.onex@gmail.com>
2016-04-17 19:53:10 +0200

    Add filter routes

--- app/routes/todos/complete.js
+++ app/routes/todos/complete.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('complete');
+    })
+  },
+  remderTemplate(controller, model) {
+    this.render('todos.inex', {
+      model: model
+    })
+  }
 });
